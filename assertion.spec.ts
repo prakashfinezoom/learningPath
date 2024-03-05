6f1848b80ac4f301efcfb535ce225e84db262c4b
prakashfinezoom <prakash.r@finezoom.com>
2024-03-05 15:25:11 +0530

    First push

---
+++ assertion.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect} from '@playwright/test';
+import loginPage from './POM/pageObjectModel';
+import * as data from "./data.json";
+ 
+test.describe('Our first script', async () => {
+    test.beforeEach(async ({page}) => {
+        await page.goto('https://practice.expandtesting.com/');
+    });
+    test('Login form', async ({page}) => {
+        await page.locator(`//a[text()='Login Form']`).click()
+        const Login = new loginPage(page)
+        await Login.fillUsername(data.testData.username)
+        await Login.fillPassword(data.testData.password)
+        await expect.soft(page.locator(`//h1[text()='Login Page']`)).toHaveText('Logi')
+        await Login.loginBtn()
+    });
+    test('Radio button', async ({page}) => 
+    {
+        await page.locator(`//a[text()='Radio Buttons']`).click();
+    })
+})
+
+
+
+
